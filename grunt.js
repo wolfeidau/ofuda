79a4af2482a0989e1ecb4451f62b1282a14445ca
Mark Wolfe <mark@wolfe.id.au>
2012-10-23 08:04:15 +1100

    Working on a rewrite at the moment.

--- grunt.js
+++ grunt.js
@@ -1,39 +1,55 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: '<json:package.json>',
-    test: {
-      files: ['test/**/*_test.js']
-    },
-    lint: {
-      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'default'
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        node: true
-      },
-      globals: {
-        exports: true
-      }
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg:'<json:package.json>',
+        test:{
+            files:['test/**/*_test.js']
+        },
+        lint:{
+            files:['grunt.js', 'lib/**/*.js', 'test/**/*.js', 'spec/**/*_test.js']
+        },
+        watch:{
+            files:'<config:lint.files>',
+            tasks:'default'
+        },
+        jshint:{
+            options:{
+                curly:true,
+                eqeqeq:true,
+                immed:true,
+                latedef:true,
+                newcap:true,
+                noarg:true,
+                sub:true,
+                undef:true,
+                boss:true,
+                eqnull:true,
+                node:true,
+                mocha:true
+            },
+            globals:{
+                exports:true
+            }
+        },
+        simplemocha: {
+            all: {
+                src: 'spec/**/*.js',
+                options: {
+                    globals: ['should'],
+                    timeout: 3000,
+                    ignoreLeaks: false,
+                    //grep: '*_test',
+                    ui: 'bdd',
+                    reporter: 'spec'
+                }
+            }
+        }
+    });
 
-  // Default task.
-  grunt.registerTask('default', 'lint test');
+    grunt.loadNpmTasks('grunt-simple-mocha');
+
+    // Default task.
+    grunt.registerTask('default', 'simplemocha');
 
 };
