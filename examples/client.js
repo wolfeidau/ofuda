772df6c5f0539c836edf41ff9079559704ab3cf6
Mark Wolfe <mark@wolfe.id.au>
2013-02-08 08:36:06 +1100

    Some tidy up of header locating code.

--- examples/client.js
+++ examples/client.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var Ofuda = require('../lib/ofuda');
 
-var ofuda = new Ofuda({headerPrefix:'Amz', hash: 'sha1', serviceLabel: 'AWS', debug: true});
+var ofuda = new Ofuda({headerPrefix: 'Amz', hash: 'sha1', serviceLabel: 'AWS', debug: true});
 
 var credentials = {accessKeyId: '44CF9590006BF252F707', accessKeySecret: 'OtxrzxIsfpFjA7SwPzILwy8Bw21TLhquhboDYROV'};
 
@@ -12,13 +12,14 @@ http_options = {
     method: 'PUT',
     headers: {
         'Content-Type': 'application/json',
-        'Content-MD5': 'ee930827ccb58cd846ca31af5faa3634'
+        'Content-MD5': 'ee930827ccb58cd846ca31af5faa3634',
+        'date': 'Thu, 07 Feb 2013 20:53:04 GMT'
     }
 };
 
 signedOptions = ofuda.signHttpRequest(credentials, http_options);
 
-var req = http.request(signedOptions, function(res) {
+var req = http.request(signedOptions, function (res) {
     console.log('STATUS: ' + res.statusCode);
     console.log('HEADERS: ' + JSON.stringify(res.headers));
 });
